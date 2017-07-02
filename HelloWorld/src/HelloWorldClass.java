ef92dcd34615a2431f009ed0ddf1b4721a6a1471
Brad V <Brad.vaile1993@hotmail.co.uk>
2017-07-02 20:20:34 +0100

    Updated:
    - Length of string
    - Reversed String

--- HelloWorld/src/HelloWorldClass.java
+++ HelloWorld/src/HelloWorldClass.java
@@ -3,7 +3,14 @@ public class HelloWorldClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World");
+		System.out.print("Hello World \n");
+		String length = "This is a test string \n";
+		
+		System.out.print("The Length is:" + length.length() + "\n");
+		
+		String backwardsString = new StringBuffer(length).reverse().toString();
+		System.out.print("Backwards:" + backwardsString);
+		
 	}
 
 }
